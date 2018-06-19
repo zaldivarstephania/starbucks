e1fffb65328dc9aeb696333b8d30972416aff464
zaldivarstephania <37090934+zaldivarstephania@users.noreply.github.com>
2018-06-19 17:50:12 -0600

    Add files via upload

---
+++ js/my-app.js
@@ -0,0 +1,49 @@
+// Initialize your app
+var myApp = new Framework7({
+	// Default title for modals
+	root:'#app',
+	theme: 'md',
+    modalTitle: 'Framework7',
+	cache:false,
+	pushState:false,
+	swipeBackPage:true,
+    swipePanel: 'left',
+	material: true,
+	routes: [
+		{
+		path: '/breakfast/',
+    	url: 'breakfast.html',
+    	name: 'breakfast',
+  		},
+		{
+		path: '/drinks/',
+    	url: 'drinks.html',
+    	name: 'drinks',
+  		},
+		{
+		path: '/home/',
+    	url: 'index.html',
+    	name: 'home',
+  		},
+		{
+		path: '/producto/',
+    	url: 'info-producto.html',
+    	name: 'producto',
+  		}
+	]
+    // ... other parameters
+});
+
+// Export selectors engine
+var $$ = Dom7;
+
+
+
+// Add another view, which is in right panel
+
+
+
+
+
+ 
+
